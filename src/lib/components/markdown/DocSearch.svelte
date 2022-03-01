fdef69600e3941c4cd724ad67fad3de8c956031d
Rahim Alwer <rahim.alwer@gmail.com>
2022-03-01 18:57:36 +1100

    prevent sidebar jumping as docsearch initializes

--- src/lib/components/markdown/DocSearch.svelte
+++ src/lib/components/markdown/DocSearch.svelte
@@ -3,10 +3,12 @@
 
 	// @ts-expect-error - no types.
 	import docsearch from '@docsearch/js';
+	import clsx from 'clsx';
 
 	import { onMount } from 'svelte';
 
 	let container;
+	let mounted = false;
 
 	onMount(() => {
 		docsearch({
@@ -20,6 +22,8 @@
 			// apiKey: '599cec31baffa4868cae4e79f180729b',
 			// indexName: 'docsearch'
 		});
+
+		mounted = true;
 	});
 </script>
 
@@ -28,3 +32,18 @@
 </svelte:head>
 
 <div bind:this={container} />
+
+<!-- For SSR to prevent sidebar jumping on initial render. -->
+<div
+	class={clsx(
+		mounted ? 'hidden' : 'flex',
+		'w-full items-center rounded-sm py-2.5 px-3 shadow-sm dark:bg-[#2d2d2d] text-[15px]',
+		'border border-gray-divider'
+	)}
+>
+	<span class="flex-1">Search</span>
+	<span class="flex space-x-0.5 font-semibold">
+		<span>⌘</span>
+		<span>K</span>
+	</span>
+</div>
