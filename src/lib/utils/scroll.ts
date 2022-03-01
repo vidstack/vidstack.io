ffd566c5caabd2b8af887940232a30f3a2965c5d
Rahim Alwer <rahim.alwer@gmail.com>
2022-03-01 12:39:34 +1100

    fix document body overflowing on menu close

--- src/lib/utils/scroll.ts
+++ src/lib/utils/scroll.ts
@@ -1,5 +1,6 @@
 export function hideDocumentScrollbar(hidden: boolean) {
 	window.requestAnimationFrame(() => {
 		document.documentElement.classList[hidden ? 'add' : 'remove']('overflow-hidden');
+		document.documentElement.classList[!hidden ? 'add' : 'remove']('overflow-x-hidden');
 	});
 }
